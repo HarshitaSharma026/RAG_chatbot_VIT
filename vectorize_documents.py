f7914751bbd56de7b97a87c7ab34ba0b6f1fe4c5
HarshitaSharma026 <harshita026.sharma@gmail.com>
2024-09-25 11:29:07 +0530

    Successful changes made to vectorize_documents file

--- vectorize_documents.py
+++ vectorize_documents.py
@@ -1,44 +1,20 @@
-from langchain_community.document_loaders import UnstructuredFileLoader
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
-from langchain_community.document_loaders import PyPDFLoader
-import asyncio
 import os
 load_dotenv()
 
-# converting pdf into text using pypdf loader
-# async def load_pdf():
-#     file_path = "./docs/Mtech_curriculum/information_security.pdf"
-#     loader = PyPDFLoader(file_path)
-#     pages = []
-#     async for page in loader.alazy_load():
-#         pages.append(page)
-#     # print(f"{pages[2].metadata}")
-#     # print(pages[2].page_content)
-
-#     store = InMemoryVectorStore.from_documents(pages, GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
-#     docs = store.similarity_search("What are discipline core courses?", k = 2)
-#     for doc in docs:
-#         print(f'Page: {doc.metadata["page"]}: {doc.page_content[:20]}\n')
-
-# def main():
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(load_pdf())
-#     loop.close()
-# if __name__ == '__main__':
-#     main()
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-def load_chunk_persis_pdf():
-    pdf_folder_path = "./docs/Mtech_curriculum"
+def load_chunk_persis_txt():
+    txt_folder_path = "./docs/Mtech_curriculum"
     documents = []
-    for file in os.listdir(pdf_folder_path):
-        if file.endswith('.pdf'):
-            pdf_path = os.path.join(pdf_folder_path, file)
-            loader = PyPDFLoader(pdf_path)
+    for file in os.listdir(txt_folder_path):
+        if file.endswith('.txt'):
+            txt_path = os.path.join(txt_folder_path, file)
+            loader = TextLoader(txt_path)
             documents.extend(loader.load())
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
     text_chunks = text_splitter.split_documents(documents)
@@ -50,7 +26,7 @@ def load_chunk_persis_pdf():
     print("Documents vectorization completed !!")
 
 def main():
-    load_chunk_persis_pdf()
+    load_chunk_persis_txt()
 
 if __name__ == "__main__":
     main()
